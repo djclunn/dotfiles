c8c3de95b6250d6c49bfc7f4a65356c1fa367108
djclunn <declunn@gmail.com>
2019-05-23 00:39:04 -0700

    better install scripts

---
+++ clone_and_curl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Vundle for vim
+git clone https://github.com/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Powerline needed?
+pip3 install powerline-status
+
+# Oh-My-Zsh install helper
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
---
+++ get_parts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Vundle for vim
+git clone https://github.com/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Oh-My-Zsh install helper
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+

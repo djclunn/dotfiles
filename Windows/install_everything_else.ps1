fd25631a4ced7e3a709bd40f6452d65935c25d32
djclunn <declunn@gmail.com>
2022-02-21 15:47:30 -0800

    improved

--- Windows/install_everything_else.ps1
+++ Windows/install_everything_else.ps1
@@ -3,13 +3,21 @@ choco install firefox -y
 choco install notepadplusplus -y
 choco install 7zip -y
 choco install python -y
-choco install vlc -y
 choco install vscode -y
-choco install winscp  -y
 choco install vim -y
 choco install everything -y
 choco install windirstat -y
 choco install github-desktop -y
 choco install pycharm-community -y
 choco install obsidian -y
+choco install git -y --params="'/NoShellIntegration'"
+choco install irfanview-she
+# no option to remove context menus for vlc in chocolatey
+# portable vlc installed in C:\ProgramData\chocolatey\bin\vlc.exe
+choco install vlc.portable -yllextension -y
+# work associated apps
 choco install slack -y
+choco install imagej -y
+choco install vnc-viewer -y
+choco install winscp -y
+# irfanview, irfanview-shellextension
